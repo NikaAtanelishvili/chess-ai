19304976e0352ad8d84ccafba957143b2cac09c5
NikaAtanelishvili <atanelishvilinika@gmail.com>
2025-06-22 18:17:27 +0400

    feat: queen move generation

--- src/movegen/movegen.py
+++ src/movegen/movegen.py
@@ -7,6 +7,7 @@ from src.movegen.bishop import Bishop
 from src.movegen.king import King
 from src.movegen.knight import Knight
 from src.movegen.pawn import Pawn
+from src.movegen.queen import Queen
 from src.movegen.rook import Rook
 
 
@@ -15,7 +16,7 @@ class MoveGenerator:
 
     piece_generators = [
         King.generate_move,
-        # Queen.generate_move,
+        Queen.generate_move,
         Rook.generate_move,
         Bishop.generate_move,
         Knight.generate_move,
@@ -24,7 +25,6 @@ class MoveGenerator:
 
     piece_data_loaders = [
         King.load_data,
-        # queen
         Rook.load_data,
         Bishop.load_data,
         Knight.load_data,
@@ -56,8 +56,10 @@ if __name__ == "__main__":
     end = time.time()
 
     start1 = time.time()
-    legal_moves = list(board.legal_moves)
+    legal_moves = MoveGenerator.generate_all_moves(board, board.turn)
     end1 = time.time()
 
     print(moves)
-    print(len(moves), f'{(end-start)-(end1 - start1):.6f}s')
+    print(len(moves), f'{(end - start):.6f}s')
+    print(len(moves), f'{(end1 - start1):.6f}s')
+
